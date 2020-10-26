7756f8bd45957c41abfbc058226d7d90f709bd2a
Robin Wacanno <robinwacco11@hotmail.com>
2020-10-26 02:20:51 +0100

    plugins/minecraft: show death messages

--- plugins/minecraft.py
+++ plugins/minecraft.py
@@ -1,7 +1,7 @@
 """
 * depends: imgflt
 """
-from core.types import Proto, OutPort, Message, Attachment, Channel, Config, Photo, JoinMessage, PartMessage, ServiceMessage, UserRequest, UserList, Metadata
+from core.types import Proto, OutPort, Message, Attachment, Channel, Config, Photo, JoinMessage, PartMessage, ServiceMessage, UserRequest, UserList, Metadata, MiscServiceMessage
 from core.bridge import Bridge
 from typing import Mapping, Any, Callable, Coroutine, Sequence, List, Dict
 import logging
@@ -19,6 +19,8 @@ CHANNEL_NAME = "chat"
 SERVER_MESSAGE_RE = re.compile(r"\[[0-9:]+\] \[Server thread/[ A-Z]+\]: (.+)")
 
 USERNAME = "[A-Za-z0-9_-]+"
+
+DEATH_MESSAGES = '(?:' + '|'.join("was shot by;was pummeled by;was pricked to death;walked into a cactus whilst trying to escape;drowned;drowned whilst trying to escape;experienced kinetic energy;experienced kinetic energy whilst trying to escape;blew up;was blown up by;was blown up by;hit the ground too hard;hit the ground too hard whilst trying to escape;fell from a high place;fell off a ladder;fell off some vines;fell off some weeping vines;fell off some twisting vines;fell off scaffolding;fell while climbing;was squashed by;went up in flames;walked into fire whilst fighting;burned to death;was burnt to a crisp whilst fighting;went off with a bang;went off with a bang due to a firework fired from;tried to swim in lava;tried to swim in lava to escape;was struck by lightning;was struck by lightning whilst fighting;discovered the floor was lava;walked into danger zone due to;was killed by magic;was killed by;was slain by;was fireballed by;was stung to death;was shot by a skull from;starved to death;suffocated in a wall;suffocated in a wall whilst fighting;was squished too much;was squashed by;was poked to death by a sweet berry bush;was poked to death by a sweet berry bush whilst trying to escape;was killed trying to hurt;was impaled by;fell out of the world;didn't want to live in the same world as;withered away".split(';')) + ')'
 
 Groups = Sequence[str]
 
@@ -99,6 +101,13 @@ class MinecraftProto(Proto):
         users_mapped = [self.user_map.get(user, user) for user in users]
         ulist = UserList(users_mapped, CHANNEL_NAME)
         await self.out_port.put_message(ulist)
+
+    @match(f"({USERNAME})( {DEATH_MESSAGES}.*)")
+    async def death(self, line:str, groups: Groups) -> None:
+        name, rest = groups
+        name_mapped = self.user_map.get(name, name)
+        msg = MiscServiceMessage(f"{name_mapped}{rest}", CHANNEL_NAME)
+        await self.out_port.put_message(msg)
 
     @match(f"{USERNAME} has made the advancement .+")
     async def advancement(self, line: str, groups: Groups) -> None:
