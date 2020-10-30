76afd7f02a1ca608782e11c5442eb1b8a61f32f4
Robin Wacanno <robinwacco11@hotmail.com>
2020-10-30 22:51:59 +0100

    plugins/telegram: import Optional

--- plugins/telegram.py
+++ plugins/telegram.py
@@ -2,7 +2,7 @@ from telethon import TelegramClient, events
 from pprint import pprint
 from core.types import Proto, OutPort, Message, Attachment, Channel, Config, Photo, JoinMessage, PartMessage, ServiceMessage, UserRequest, UserList, Metadata, MiscServiceMessage
 from core.bridge import Bridge
-from typing import Mapping, Any
+from typing import Mapping, Any, Optional
 import asyncio
 from io import BytesIO
 import logging
