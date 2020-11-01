2693d379813487346ad36bd01d3f16f78ab0e3d9
Robin Wacanno <robinwacco11@hotmail.com>
2020-11-01 02:59:44 +0100

    pre-commit: exit prematurely if there are no staged changes

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,6 +8,12 @@ pushd "$TOP_LEVEL" 2>&1 1>/dev/null
 # This way, mypy runs against the state of the repo as it is going to be committed.
 # --no-verify inhibits the pre-commit hook so we don't loop infinitely
 git commit --no-verify -m 'Save index' 2>&1 1>/dev/null
+if [ $? -eq 1 ]
+then
+    echo "No staged changes."
+    popd 2>&1 1>/dev/null
+    exit 1
+fi
 git stash push -u -m 'Unstaged changes and untracked files' 2>&1 1>/dev/null
 git reset --soft HEAD^ 2>&1 1>/dev/null
 
