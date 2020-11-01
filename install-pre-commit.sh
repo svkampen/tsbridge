6e109f1425f230cd443a0a111464227b15fd20b7
Sam van Kampen <sam@tehsvk.net>
2020-11-01 01:53:40 +0000

    git: add pre-commit hook
    The new pre-commit hook automatically runs mypy, checking whether we are
    not committing any code that does not static analysis.

---
+++ install-pre-commit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -x
+ln -s ../../pre-commit.sh .git/hooks/pre-commit
---
+++ pre-commit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# pre-commit script which runs mypy and (optionally) tests
+
+TOP_LEVEL=$(git rev-parse --show-toplevel)
+pushd "$TOP_LEVEL" 2>&1 1>/dev/null
+
+# This three-command dance stashes unstaged changes + untracked files.
+# This way, mypy runs against the state of the repo as it is going to be committed.
+# --no-verify inhibits the pre-commit hook so we don't loop infinitely
+git commit --no-verify -m 'Save index' 2>&1 1>/dev/null
+git stash push -u -m 'Unstaged changes and untracked files' 2>&1 1>/dev/null
+git reset --soft HEAD^ 2>&1 1>/dev/null
+
+mypy core plugins
+
+EXIT_CODE=0
+
+if [ $? -eq 1 ]
+then
+	echo "Error during mypy execution, aborting commit..."
+	EXIT_CODE=1
+fi
+
+git stash pop 2>&1 1>/dev/null
+popd 2>&1 1>/dev/null
+
+exit $EXIT_CODE
