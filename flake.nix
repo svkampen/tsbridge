4fef6b95ba5d06a97c4f229381b0b379ee49f27a
Sam van Kampen <sam@tehsvk.net>
2025-08-18 20:11:37 +0000

    override build systems for pyaes and docopt

--- flake.nix
+++ flake.nix
@@ -34,11 +34,25 @@
           sourcePreference = "wheel";
         };
 
+        pyprojectOverrides = final: prev: {
+          pyaes = prev.pyaes.overrideAttrs (old: {
+            nativeBuildInputs = old.nativeBuildInputs ++ [
+              (final.resolveBuildSystem { setuptools = []; })
+            ];
+          });
+          docopt = prev.docopt.overrideAttrs (old: {
+            nativeBuildInputs = old.nativeBuildInputs ++ [
+              (final.resolveBuildSystem { setuptools = []; })
+            ];
+          });
+        };
+
         python = pkgs.python312;
         pythonSet = (pkgs.callPackage pyproject-nix.build.packages { inherit python; }).overrideScope(
           lib.composeManyExtensions [
             pyproject-build-systems.overlays.default
             overlay
+            pyprojectOverrides
           ]);
       in
       {
@@ -57,7 +71,7 @@
             // lib.optionalAttrs pkgs.stdenv.isLinux {
               # Python libraries often load native shared objects using dlopen(3).
               # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+              # LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
             };
           shellHook = ''
             unset PYTHONPATH
